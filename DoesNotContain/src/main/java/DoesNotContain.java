16f02d4674df11dec18991943eb16d936df03045
ItsLaz <yesimandriy@gmail.com>
2023-01-01 19:48:05 +0000

    Saved progress on DoesNotContain 2023-01-01 19:48:05 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -13,6 +13,16 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        return false;
+        int c = 0;
+        for (int i = 0;i < arr.length; i++) {
+            if(arr[i] == target){
+                c++;
+            }
+        }
+        if(c == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
