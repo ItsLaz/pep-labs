330883f4002bd1c8b3fe6e51f10f0790d0e128f4
ItsLaz <yesimandriy@gmail.com>
2023-01-01 22:21:00 +0000

    Saved progress on RequestBody 2023-01-01 22:21:00 +0000

--- RequestBody/src/main/java/com/revature/JavalinSingleton.java
+++ RequestBody/src/main/java/com/revature/JavalinSingleton.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.javalin.Javalin;
 
 /**
@@ -17,7 +19,13 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem1", ctx -> {
-                //implement logic here
+                String jsonString = ctx.body();
+                ObjectMapper om = new ObjectMapper();
+                Song song = om.readValue(jsonString, Song.class);
+                ctx.contentType("application/json"); 
+                String aName = song.getArtistName();
+                String jsonStringToBeReturned = om.writeValueAsString(aName);
+                ctx.result(aName);
         });
 
         /**
@@ -28,7 +36,13 @@ public class JavalinSingleton {
          * Note: Please refer to the "RequestBody.MD" file for more assistance if needed.
          */
         app.post("/problem2", ctx -> {
-               //implement logic here
+            String jsonString = ctx.body();
+            ObjectMapper om = new ObjectMapper();
+            Song song = om.readValue(jsonString, Song.class);
+            ctx.contentType("application/json"); 
+            song.setArtistName("Beatles");
+            String jsonStringToBeReturned = om.writeValueAsString(song);
+            ctx.result(jsonStringToBeReturned);
         });
 
 
