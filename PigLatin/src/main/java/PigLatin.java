a3baac7b8d46ec1d467714551545e33d2819a845
ItsLaz <yesimandriy@gmail.com>
2023-01-05 22:10:13 +0000

    Saved progress on PigLatin 2023-01-05 22:10:13 +0000

--- PigLatin/src/main/java/PigLatin.java
+++ PigLatin/src/main/java/PigLatin.java
@@ -17,6 +17,8 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String f = in.substring(0, 1);
+        String nof = in.substring(1, in.length());
+        return nof+f+"ay";
     }
 }
